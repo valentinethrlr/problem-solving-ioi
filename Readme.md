13c7f4fde748b2c56d36715d8136abc2516999f7
Cédric Donner <cedonner@gmail.com>
2023-09-05 14:06:43 +0200

    init commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Environnement pour résoudre les problèmes des IOI
+
+## Mode d'emploi
+
+- Copier le dossier `template-problem` et le renommer
+- éditer le code dans le fichier `solve.py` pour résoudre votre problème
+- Utilisez les fichiers dans le dossier `tests` pour déboguer votre programme sur des tests spécifiques
+- Copiez ce code dans France-IOI pour l'évaluer
+
+
---
+++ template-problem/solve.py
@@ -0,0 +1,59 @@
+'''
+
+Template for France-IOI programming tasks with debugger in gitpod
+
+The Python version on gitpod is 3.11, whereas the python version on france-ioi
+is sadly stuck at 3.4.2 ... This means no type hints, no dataclasses, ... The
+code should therefore remain pretty basic to run flawlessly on france-ioi
+
+'''
+
+##################################################################
+# read input from file tests/test1.in as if type on the keyboard
+# This shouldn't run on France-IOI
+# replace this with the name of your test file
+test_file = 'test1.in'
+
+import sys, os, platform
+# only if executed on Python 3.11 (gitpod), will be false on france-ioi
+if platform.python_version_tuple()[:2] == ('3', '11'):
+    os.chdir(os.path.dirname(__file__))
+    sys.stdin = open(os.path.join('tests', test_file), "r")
+##################################################################
+
+
+
+from collections import namedtuple
+
+Problem = namedtuple(Problem, [])
+
+    
+
+
+def parse_input():
+    '''
+    
+    Parses the input data and returns a dictionary with everything
+    well structured.
+    
+    '''
+    return Problem()
+
+def solve(problem):
+    result = []
+    
+    return result
+        
+    
+    
+def output(result):
+    for r in result:
+        print(r)
+    
+            
+
+
+if __name__ == '__main__':
+    problem = parse_input()
+    result = solve(problem)
+    output(result)
